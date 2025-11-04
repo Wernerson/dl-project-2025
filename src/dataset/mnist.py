2b3ec7e1567032c99a09bc8cd2391a40fb30e99d
Sebastian Brunner <brunner.sebi4@gmail.com>
2025-11-04 21:00:02 +0100

    add loggers

--- src/dataset/mnist.py
+++ src/dataset/mnist.py
@@ -2,7 +2,7 @@ import torch.utils.data as data
 import torchvision as tv
 
 
-def mnist(train_val_split):
-    dataset = tv.datasets.MNIST("./data", download=True, transform=tv.transforms.ToTensor())
+def mnist(data_dir, train_val_split):
+    dataset = tv.datasets.MNIST(data_dir, download=True, transform=tv.transforms.ToTensor())
     train, val = data.random_split(dataset, train_val_split)
     return data.DataLoader(train), data.DataLoader(val)
--- src/main.py
+++ src/main.py
@@ -9,9 +9,10 @@ def main(cfg):
     if cfg.get("seed"):
         L.seed_everything(cfg.seed, workers=True)
 
+    logger = instantiate(cfg.logger)
     train, val = call(cfg.dataset)
     model = instantiate(cfg.model)
-    trainer = L.Trainer()
+    trainer = L.Trainer(logger=logger)
     trainer.fit(model, train, val)
 
 
