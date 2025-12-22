e7f3cd7c97cc12e2a1eceffe095f24fc626b237f
Sebastian Brunner <brunner.sebi4@gmail.com>
2025-12-22 18:16:38 +0100

    add hydra errors

--- run.sh
+++ run.sh
@@ -10,4 +10,4 @@ fi
 . /etc/profile.d/modules.sh
 module add cuda/12.9
 source .venv/bin/activate
-python3 src/main.py +experiment=$1 +run=cluster
+HYDRA_FULL_ERROR=1 python3 src/main.py +experiment=$1 +run=cluster
