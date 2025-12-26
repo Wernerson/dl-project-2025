fdb2e9bfa306add1dffd57c7b07f532c741cfc32
Sebastian Brunner <brunner.sebi4@gmail.com>
2025-12-26 14:17:20 +0100

    adjust sample generation during training

--- src/callbacks/generation.py
+++ src/callbacks/generation.py
@@ -1,13 +1,16 @@
-import numpy as np
+import os
+from pathlib import Path
+
 import torch
 from lightning import Callback
-from symusic import Synthesizer, BuiltInSF3
+from symusic import Synthesizer, dump_wav, BuiltInSF3
 
 
 class SampleGeneration(Callback):
-    def __init__(self, tokenizer, sample_rate=44100, num_samples=3, quality=4):
+    def __init__(self, tokenizer, sample_dir, sample_rate=44100, num_samples=3, quality=4):
         super(Callback, self).__init__()
         self.tokenizer = tokenizer
+        self.sample_dir = sample_dir
         self.sample_rate = sample_rate
         self.num_samples = num_samples
 
@@ -38,12 +41,18 @@ class SampleGeneration(Callback):
                     midi_obj = midi_obj.clip(0, 10)
 
                 # We render. If it fails due to size, the try-except below catches it.
-                audio_data = self.synthesizer.render(midi_obj, stereo=True)
-                audio_np = np.ravel(np.array(audio_data))
+                audio = self.synthesizer.render(midi_obj, stereo=True)
+
+                sample_dir = Path(self.sample_dir)
+                if not os.path.exists(sample_dir):
+                    os.makedirs(sample_dir)
+                file = sample_dir / f"epoch={trainer.current_epoch}_sample_{i}.wav"
+
+                dump_wav(str(file), audio, sample_rate=self.sample_rate)
 
                 # log to WandB
                 trainer.logger.log_audio(
-                    "val/samples", [audio_np],
+                    "val/samples", [file],
                     sample_rate=[self.sample_rate],
                     step=trainer.current_epoch
                 )
