00287c4f389a3959f0b86e7cc887929704f4b87d
Sebastian Brunner <brunner.sebi4@gmail.com>
2026-01-10 09:57:52 +0100

    comment out eval

--- src/main.py
+++ src/main.py
@@ -30,7 +30,7 @@ def main(cfg):
     dataset = instantiate(cfg.dataset)
     model = instantiate(cfg.model)
     trainer = instantiate(cfg.trainer, logger=logger)
-    evaluator = instantiate(cfg.evaluator)
+    # evaluator = instantiate(cfg.evaluator)
 
     print("\nTraining...")
     trainer.fit(model, datamodule=dataset)
@@ -38,8 +38,8 @@ def main(cfg):
     print("\nTesting...")
     trainer.test(model, datamodule=dataset)
 
-    print("\nEvaluating...")
-    evaluator.evaluate(model)
+    # print("\nEvaluating...")
+    # evaluator.evaluate(model)
 
 
 if __name__ == "__main__":
