aa84a6f412a22c0e7eb9b7496be537332da87349
Sebastian Brunner <brunner.sebi4@gmail.com>
2026-01-13 09:04:12 +0100

    remvoe eval from training

--- src/main.py
+++ src/main.py
@@ -29,16 +29,12 @@ def main(cfg):
     dataset = instantiate(cfg.dataset)
     model = instantiate(cfg.model)
     trainer = instantiate(cfg.trainer, logger=logger)
-    evaluator = instantiate(cfg.evaluator)
 
     print("\nTraining...")
     trainer.fit(model, datamodule=dataset)
 
     print("\nTesting...")
     trainer.test(model, datamodule=dataset)
-
-    print("\nEvaluating...")
-    evaluator.evaluate(model)
 
 if __name__ == "__main__":
     main()
